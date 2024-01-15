64649bfd243cc81f611912c1ab59e008ea02710e
Maxwell Rubert <137780966+Maxwellrubert@users.noreply.github.com>
2024-01-16 00:50:34 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Prompt the user for their name
+var userName = prompt("What's your name?");
+
+// Check if the user entered a name
+if (userName) {
+  // Display a personalized greeting
+  alert("Hello, " + userName + "! Welcome to the simple JavaScript program.");
+} else {
+  // Display a generic greeting if no name is entered
+  alert("Hello there! Welcome to the simple JavaScript program.");
+}
